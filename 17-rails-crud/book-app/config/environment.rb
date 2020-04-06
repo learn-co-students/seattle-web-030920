6b76fe488a876b5c859205fc2bca4da818c4c081
Flatiron School <soundarya.rajendran@flatironschool.com>
2020-04-06 11:35:02 -0700

    Adding lecture code

---
+++ 17-rails-crud/book-app/config/environment.rb
@@ -0,0 +1,6 @@
+# Load the Rails application.
+require_relative 'application'
+require_relative '../app/adapters/google_books'
+
+# Initialize the Rails application.
+Rails.application.initialize!
---
+++ 17-rails-crud/book-app/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # resources :books
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'books', to: "books#index"
+  get '/books/new', to: "books#new"
+  get '/books/:id', to: "books#show", as: "book"
+  post '/books', to: "books#create"
+  get '/books/:id/edit', to: "books#edit"
+  patch '/books/:id', to: "books#update"
+end

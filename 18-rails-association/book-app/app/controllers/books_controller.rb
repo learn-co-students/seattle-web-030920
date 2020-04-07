f5980c9710c08db46653e9fa080277a1e2734168
Flatiron School <soundarya.rajendran@flatironschool.com>
2020-04-07 10:00:35 -0700

    Adding code

---
+++ 18-rails-association/book-app/app/controllers/books_controller.rb
@@ -0,0 +1,42 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update]
+
+  def index
+    @books = Book.all
+  end
+
+  # def show
+  #   # byebug
+  #   # set_params
+  # end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    # byebug
+    book = Book.create(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  # def edit
+  #   # set_params
+  # end
+
+  def update
+    # set_params
+    @book.update(book_params)
+      redirect_to book_path(@book.id)
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :snippet)
+  end
+end
